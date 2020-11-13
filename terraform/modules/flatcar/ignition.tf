fdbdb13f234e126d68f5de0559d583318785e23b
Karl Skewes <karl.skewes@gmail.com>
2020-11-13 14:35:16 +1300

    feat(terraform): add flatcar linux libvirt kvm module

---
+++ terraform/modules/flatcar/ignition.tf
@@ -0,0 +1,71 @@
+data "ignition_config" "ignition" {
+  count = length(var.ignition_config) > 0 ? 0 : var.guest_count
+
+  users = [
+    data.ignition_user.core[0].rendered,
+  ]
+
+  files = [
+    element(data.ignition_file.hostname.*.rendered, count.index)
+  ]
+
+  networkd = [
+    data.ignition_networkd_unit.network-dhcp[0].rendered,
+  ]
+
+  systemd = [
+    data.ignition_systemd_unit.etcd-member[count.index].rendered,
+  ]
+
+}
+
+data "ignition_file" "hostname" {
+  count = length(var.ignition_config) > 0 ? 0 : var.guest_count
+
+  filesystem = "root"
+  path       = "/etc/hostname"
+  mode       = 420
+
+  content {
+    content = "${var.guest_hostname}-${format("%02d", count.index)}"
+  }
+}
+
+data "ignition_user" "core" {
+  count = length(var.ignition_config) > 0 ? 0 : 1
+  name  = "core"
+
+  ssh_authorized_keys = [var.guest_user_ssh_authorized_key]
+}
+
+data "ignition_networkd_unit" "network-dhcp" {
+  count   = length(var.ignition_config) > 0 ? 0 : 1
+  name    = "00-wired.network"
+  content = file("${path.module}/units/00-wired.network")
+}
+
+data "ignition_systemd_unit" "etcd-member" {
+  count   = length(var.ignition_config) > 0 ? 0 : 1
+  name    = "etcd-member.service"
+  enabled = true
+  dropin {
+    content = data.template_file.etcd-member[count.index].rendered
+    name    = "20-etcd-member.conf"
+  }
+}
+
+resource "random_string" "token" {
+  count   = length(var.ignition_config) > 0 ? 0 : 1
+  length  = 16
+  special = false
+}
+
+data "template_file" "etcd-member" {
+  count    = length(var.ignition_config) > 0 ? 0 : 1
+  template = file("${path.module}/units/20-etcd-member.conf")
+  vars = {
+    node_name     = "${var.guest_hostname}-${format("%02d", count.index)}"
+    private_ip    = "${var.guest_network_address_prefix}${format("%01d", count.index)}"
+    cluster_token = random_string.token[0].result
+  }
+}
---
+++ terraform/modules/flatcar/main.tf
@@ -0,0 +1,75 @@
+# Qcow2 base OS volume that can be shared by VM's (domain's)
+resource "libvirt_volume" "os_base" {
+  name   = "${var.guest_hostname}-os_base"
+  source = var.guest_os_volume_source
+}
+
+# OS volume per guest VM
+resource "libvirt_volume" "os" {
+  count          = var.guest_count
+  name           = "${var.guest_hostname}-${format("%02d", count.index)}-os.qcow2"
+  base_volume_id = libvirt_volume.os_base.id
+  pool           = var.guest_pool_name
+  size           = var.guest_os_volume_size
+}
+
+# Secondary volume per guest VM
+# resource "libvirt_volume" "secondary" {
+#   count = var.guest_count
+#   name  = "${var.guest_hostname}-${format("%02d", count.index)}-secondary.qcow2"
+#   pool  = "usb${format("%02d", count.index)}"
+#   size  = 100000000000
+# }
+
+
+resource "libvirt_ignition" "ignition" {
+  count   = var.guest_count
+  name    = "${var.guest_hostname}-${format("%02d", count.index)}-ignition"
+  pool    = var.guest_pool_name
+  content = length(var.ignition_config) > 0 ? var.ignition_config : element(data.ignition_config.ignition.*.rendered, count.index)
+}
+
+# domain/vm's to create
+resource "libvirt_domain" "domain_vm" {
+  count  = var.guest_count
+  name   = "${var.guest_hostname}-${format("%02d", count.index)}"
+  memory = var.guest_memory
+  vcpu   = var.guest_cpu
+
+  coreos_ignition = element(libvirt_ignition.ignition.*.id, count.index)
+  fw_cfg_name     = "opt/org.flatcar-linux/config"
+
+  network_interface {
+    mac    = "${var.guest_network_mac_base}:${format("%02d", var.guest_network_mac_offset + count.index)}"
+    bridge = var.guest_network_interface
+    # requires qemu-agent but times out for me.
+    # wait_for_lease = true
+  }
+
+  boot_device {
+    dev = ["hd", "network"]
+  }
+
+  # IMPORTANT
+  # Ubuntu can hang is a isa-serial is not present at boot time.
+  # If you find your CPU 100% and never is available this is why
+  console {
+    type        = "pty"
+    target_port = "0"
+    target_type = "serial"
+  }
+
+  console {
+    type        = "pty"
+    target_type = "virtio"
+    target_port = "1"
+  }
+
+  disk {
+    volume_id = element(libvirt_volume.os.*.id, count.index)
+  }
+  # disk {
+  #   volume_id = element(libvirt_volume.secondary.*.id, count.index)
+  # }
+}
+
